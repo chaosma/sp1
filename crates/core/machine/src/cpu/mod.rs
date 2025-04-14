f9d79438e9e8c18c7a20cc1a9776c23a2626a0c5
Chao Ma <mc8305@gmail.com>
2025-04-14 09:11:52 +0000

    fix default shape issue for large shard size

--- crates/core/machine/src/cpu/mod.rs
+++ crates/core/machine/src/cpu/mod.rs
@@ -3,7 +3,7 @@ pub mod columns;
 pub mod trace;
 
 /// The maximum log degree of the CPU chip to avoid lookup multiplicity overflow.
-pub const MAX_CPU_LOG_DEGREE: usize = 22;
+pub const MAX_CPU_LOG_DEGREE: usize = 23;
 
 /// A chip that implements the CPU.
 #[derive(Default)]
