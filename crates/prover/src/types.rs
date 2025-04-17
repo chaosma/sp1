634390956c000373eace8f160db155ee3fd2985c
Chao Ma <mc8305@gmail.com>
2025-04-17 18:30:25 +0800

    (wip) compress_proofs

--- crates/prover/src/types.rs
+++ crates/prover/src/types.rs
@@ -131,6 +131,21 @@ pub struct SP1ProofWithMetadata<P: Clone> {
     pub cycles: u64,
 }
 
+/// Input for recursion prover
+#[derive(Serialize, Deserialize, Clone)]
+pub enum RecursionInput {
+    // first layer input from core
+    Single { vk: StarkVerifyingKey<CoreSC>, proof: ShardProof<CoreSC> },
+    // intermediate layer input
+    Double { vks_and_proofs: [(StarkVerifyingKey<InnerSC>, ShardProof<InnerSC>); 2] },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SP1ProofMetadata {
+    pub stdin: SP1Stdin,
+    pub public_values: SP1PublicValues,
+}
+
 impl<P: Serialize + DeserializeOwned + Clone> SP1ProofWithMetadata<P> {
     pub fn save(&self, path: impl AsRef<Path>) -> Result<()> {
         bincode::serialize_into(File::create(path).expect("failed to open file"), self)
